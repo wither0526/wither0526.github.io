6399f33783ecef845ed8964f8c92258a40634190
wither0526 <507749421@qq.com>
2024-10-28 16:21:28 +0800

    Site updated: 2024-10-28 16:21:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/22/hello-world/","2024/10/28/myfirstblog/"];function toRandomPost(){
+var posts=["2024/10/28/myfirstblog/","2024/10/22/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
