e51d05b5beb4da6b4194ce56889c0fcf3f48d987
wither <507749421@qq.com>
2025-04-16 20:20:56 +0800

    Site updated: 2025-04-16 20:20:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/30/learning-lauguage-c/","2024/10/22/myfirstblog/","2024/10/22/rm_vision/","2025/01/01/summary_of_2024/","2025/04/10/sentry_embedded_2025/","2024/10/30/machine-learning/","2025/01/23/winter_vacation_plan/"];function toRandomPost(){
+var posts=["2024/10/22/myfirstblog/","2025/04/10/sentry_embedded_2025/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
