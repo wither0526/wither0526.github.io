a0e3fa4acfde110f8352dc3ac4c092106c1a09a4
wither <507749421@qq.com>
2025-05-22 15:52:48 +0800

    Site updated: 2025-05-22 15:52:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/10/sentry_embedded_2025/","2024/10/22/myfirstblog/","2025/04/10/sentry_machine_2025/"];function toRandomPost(){
+var posts=["2024/10/22/myfirstblog/","2025/04/10/sentry_machine_2025/","2025/04/10/sentry_embedded_2025/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
